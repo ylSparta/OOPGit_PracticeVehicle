73f3221366a8f73a7ba37e12e502816ba49ad0bf
YangaL96 <ylomuro12@gmail.com>
2020-12-31 20:53:28 +0000

    Create intial project

---
+++ OOPGit_PrcaticeVehicle/OOPGit_PrcaticeVehicle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPGit_PrcaticeVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
